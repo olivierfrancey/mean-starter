8bf592db0cd6f17b4c42decfd223deeace7d7d48
Olivier Francey <olivier.francey@gmail.com>
2018-03-17 08:56:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# MEAN-starter
+
+Full stack web server based on node.js-Angular5-Express-MongoDB
+
+## Installation
+```prompt
+$ git clone xxxx your-app-name
+$ cd your-app-name
+$ npm i
+```
+
+## Dev
+Start node.js server with
+```prompt
+$ nodemon
+````
+
+Start angular server with
+```prompt
+$ ng serve
+```
+
+## Angular client
+The angular client contents the following assets:
+- material.angular
+- routing file
+
---
+++ client/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
